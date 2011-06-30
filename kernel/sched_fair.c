40ffff6ad0e0b97008f475f0d05c977480b011b0
Jeff Ohlstein <johlstei@codeaurora.org>
2011-06-30 12:11:24 -0700

    sched_fair: Remove cpu from nohz idle set if not active
    Currently, when determining which cpu should do load balancing in nohz
    mode, we still consider cpus that have been hotremoved. This is a
    problem because, if we decide to try and move the load balancing to a
    hotremoved cpu, we will spin and stop doing load balancing at all. This
    will also make us cease doing any idle power collapse until the cpu has
    been brought back online.
    
    Fix this by removing a cpu from the nohz idle mask if we find that it
    isn't active when we consider it for load balancing.
    
    Change-Id: I93a86fdcab88964a452cb3f38e4815db13d0ee6c
    CRs-Fixed: 290042
    Signed-off-by: Jeff Ohlstein <johlstei@codeaurora.org>
    Signed-off-by: Raj Kushwaha <rajk@codeaurora.org>

--- kernel/sched_fair.c
+++ kernel/sched_fair.c
@@ -3314,8 +3314,10 @@ int select_nohz_load_balancer(int stop_tick)
 		cpu_rq(cpu)->in_nohz_recently = 1;
 
 		if (!cpu_active(cpu)) {
-			if (atomic_read(&nohz.load_balancer) != cpu)
+			if (atomic_read(&nohz.load_balancer) != cpu) {
+				cpumask_clear_cpu(cpu, nohz.cpu_mask);
 				return 0;
+			}
 
 			/*
 			 * If we are going offline and still the leader,
@@ -3324,6 +3326,7 @@ int select_nohz_load_balancer(int stop_tick)
 			if (atomic_cmpxchg(&nohz.load_balancer, cpu, -1) != cpu)
 				BUG();
 
+			cpumask_clear_cpu(cpu, nohz.cpu_mask);
 			return 0;
 		}
 
