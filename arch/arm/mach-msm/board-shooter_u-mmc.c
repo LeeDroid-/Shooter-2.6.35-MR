27b89eaae414c796790d4e2de74d8de646a5d1cf
LeeDroid- <leedroid.lb@gmail.com>
2011-10-20 23:30:01 +0100

    Lower WiFi voltages

--- arch/arm/mach-msm/board-shooter_u-mmc.c
+++ arch/arm/mach-msm/board-shooter_u-mmc.c
@@ -155,7 +155,7 @@ static unsigned int shooter_u_wifi_status(struct device *dev)
 }
 
 static struct mmc_platform_data shooter_u_wifi_data = {
-        .ocr_mask               = MMC_VDD_28_29,
+        .ocr_mask               = MMC_VDD_24_25,
         .status                 = shooter_u_wifi_status,
         .register_status_notify = shooter_u_wifi_status_register,
         .embedded_sdio          = &shooter_u_wifi_emb_data,
