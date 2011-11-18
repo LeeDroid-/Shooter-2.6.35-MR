58f12ae7ae7f890cc16d756af5367601059ab76e
Jeff Ohlstein <johlstei@codeaurora.org>
2011-11-18 15:46:56 -0800

    msm: watchdog: increase pet time to ten seconds
    Petting the watchdog every three seconds has impacts on power
    consumption. Increase this to ten seconds, and increase the bark and
    bite times for the simulator and otherwise accordingly.
    
    CRs-Fixed: 317705
    Signed-off-by: Jeff Ohlstein <johlstei@codeaurora.org>
    (cherry picked from commit 120aac4c1ea78411bce03826ffec05f52eaaf5a)
    
    Conflicts:
    
    arch/arm/mach-msm/msm_watchdog.c
    
    Change-Id: Id735b5934050fad536181893d19efc1c28297d31
    Signed-off-by: Jeff Ohlstein <johlstei@codeaurora.org>
    Signed-off-by: swetha <sbasin@codeaurora.org>

--- arch/arm/mach-msm/msm_watchdog.c
+++ arch/arm/mach-msm/msm_watchdog.c
@@ -43,7 +43,7 @@
 #define WDT0_BITE_TIME	(MSM_TMR0_BASE + 0x5C)
 
 /* Watchdog pet interval in ms */
-#define PET_DELAY 3000
+#define PET_DELAY 10000
 static unsigned long delay_time;
 static unsigned long long last_pet;
 static unsigned long forced_pets;
@@ -315,8 +315,8 @@ static int __init init_watchdog(void)
 	delay_time = msecs_to_jiffies(PET_DELAY);
 
 	/* 32768 ticks = 1 second */
-	writel(32768*8, WDT0_BARK_TIME);
-	writel(32768*9, WDT0_BITE_TIME);
+	writel(32768*11, WDT0_BARK_TIME);
+	writel(32768*12, WDT0_BITE_TIME);
 
 	queue_delayed_work(msm_watchdog_wq, &dogwork_struct, delay_time);
 
