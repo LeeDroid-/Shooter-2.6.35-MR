9a09749e367eeabe0341c5b62d92dc560e9b9419
LeeDrOiD <leedroid.lb@gmail.com>
2012-01-24 20:18:42 +0000

    scaling_available_governors: fix permissions

--- drivers/cpufreq/cpufreq.c
+++ drivers/cpufreq/cpufreq.c
@@ -747,7 +747,7 @@ cpufreq_freq_attr_ro_perm(cpuinfo_cur_freq, 0400);
 cpufreq_freq_attr_ro(cpuinfo_min_freq);
 cpufreq_freq_attr_ro(cpuinfo_max_freq);
 cpufreq_freq_attr_ro(cpuinfo_transition_latency);
-cpufreq_freq_attr_ro(scaling_available_governors);
+cpufreq_freq_attr_ro_perm(scaling_available_governors, 0444);
 cpufreq_freq_attr_ro(scaling_driver);
 cpufreq_freq_attr_ro_perm(scaling_cur_freq, 0444);
 cpufreq_freq_attr_ro(bios_limit);
