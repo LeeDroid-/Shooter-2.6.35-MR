9116d64476cd3b68d9b2027b81388aefc5edb84c
LeeDrOiD <leedroid.lb@gmail.com>
2012-02-14 12:48:01 +0000

    Lower virtual key backlight voltage

--- arch/arm/mach-msm/board-shooter_u.c
+++ arch/arm/mach-msm/board-shooter_u.c
@@ -2741,7 +2741,7 @@ static struct pm8058_led_config pm_led_config_xb[] = {
 		.duites_size = 8,
 		.duty_time_ms = 32,
 		.lut_flag = PM_PWM_LUT_RAMP_UP | PM_PWM_LUT_PAUSE_HI_EN,
-		.out_current = 20,
+		.out_current = 2,
 	},
 	{
 		.name = "green",
