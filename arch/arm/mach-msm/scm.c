dcd8f675e886b7efb05d2e31e065cf2ec40bc6e4
LeeDrOiD <leedroid.lb@gmail.com>
2012-02-16 02:22:50 +0000

    SCM Compiler fixup

--- arch/arm/mach-msm/scm.c
+++ arch/arm/mach-msm/scm.c
@@ -322,6 +322,7 @@ u32 scm_call_atomic1(u32 svc, u32 cmd, u32 arg1)
 		__asmeq("%1", "r0")
 		__asmeq("%2", "r1")
 		__asmeq("%3", "r2")
+		".arch_extension sec\n"
 		"smc	#0	@ switch to secure world\n"
 		: "=r" (r0)
 		: "r" (r0), "r" (r1), "r" (r2)
@@ -354,6 +355,7 @@ u32 scm_call_atomic2(u32 svc, u32 cmd, u32 arg1, u32 arg2)
 		__asmeq("%2", "r1")
 		__asmeq("%3", "r2")
 		__asmeq("%4", "r3")
+		".arch_extension sec\n"
 		"smc	#0	@ switch to secure world\n"
 		: "=r" (r0)
 		: "r" (r0), "r" (r1), "r" (r2), "r" (r3));
